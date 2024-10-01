aca2d69dbf1db036571c4c2d7b62edc921d258bf
bruneuskaya <s.brunevskaya@finyard.com>
2024-10-01 14:52:58 +0300

    main-1: updated file server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app=express()
 const PORT= process.env.PORT || 8000;
 
@@ -11,10 +12,31 @@ app.get('/', (req, res)=>{
     res.send('hi')
 })
 
+let votes = {
+    1: 0,
+    2: 0,
+    3: 0
+};
+
 app.post('/submit', (req, res) => {
     const { selectedOption } = req.body;
-    console.log(`Выбранный вариант: ${selectedOption}`);
-    res.json({ message: `Выбранный вариант: ${selectedOption}` });
+    if (votes[selectedOption] !== undefined) {
+        votes[selectedOption] += 1;
+    }
+
+    const statistics = {
+        option1: votes[1],
+        option2: votes[2],
+        option3: votes[3]
+    };
+
+    fs.writeFile(path.json(__dirname, 'statistics.json'), JSON.stringify(statistics, null, 2), (err)=>{
+        if (err) {
+            console.error('Ошибка при записи в файл:', err);
+            return res.status(500).json({ error: 'Ошибка при записи данных' });
+        }
+        res.json(statistics);
+    })
 });
 
 app.listen(PORT, () => {
